aa78ab323f678dab5dc3e60f16b6e450d3fb61d6
mathlander <daniel.uribe@gmail.com>
2020-05-15 18:59:44 -0500

    Added NewTimelineCard, consumed in TimelinesPage.tsx.

---
+++ penman-ui/src/components/timeline/NewTimelineCard.tsx
@@ -0,0 +1,145 @@
+import React, { Component, ChangeEvent, MouseEvent } from 'react';
+import { connect, ConnectedProps } from 'react-redux';
+import { IRootState, IAuthenticatedUser, ITimeline } from '../../store/types';
+import { create } from '../../store/actions/timelineActions';
+
+const mapStateToProps = (state: IRootState) => {
+    return {
+        authenticatedUser: state.auth.authenticatedUser,
+        timelineErrorState: state.timeline.timelineErrorState,
+        isOffline: state.offline.isOffline,
+    };
+};
+
+const mapDispatchToProps = (dispatch: any) => {
+    return {
+        create: (user: IAuthenticatedUser, newTimeline: ITimeline, suppressTimeoutAlert: boolean) => dispatch(create(user, newTimeline, suppressTimeoutAlert)),
+    };
+};
+
+const localConnector = connect(mapStateToProps, mapDispatchToProps);
+const now = new Date();
+
+type PropsFromRedux = ConnectedProps<typeof localConnector>;
+type Props = PropsFromRedux;
+
+interface INewTimelineCardState {
+    eventStartPicker: M.Datepicker | null;
+    eventEndPicker: M.Datepicker | null;
+    eventStartInputElement: HTMLInputElement | null;
+    eventEndInputElement: HTMLInputElement | null;
+    title: string;
+    eventStart: Date;
+    eventEnd: Date;
+};
+
+class NewTimelineCard extends Component<Props> {
+    state: INewTimelineCardState = {
+        eventStartPicker: null,
+        eventEndPicker: null,
+        eventStartInputElement: null,
+        eventEndInputElement: null,
+        title: '',
+        eventStart: now,
+        eventEnd: now,
+    }
+
+    componentDidMount() {
+        const eventStartElements = document.querySelectorAll(`.datepicker-newtimeline-cc-event-start`);
+        const eventEndElements = document.querySelectorAll(`.datepicker-newtimeline-cc-event-end`);
+        const setStateProxyFn = (key: string, date: Date) => this.setState({ [key]: date });
+        const startCallback = (selectedDate: Date) => setStateProxyFn('eventStart', selectedDate);
+        const endCallback = (selectedDate: Date) => setStateProxyFn('eventEnd', selectedDate);
+        const eventStartPicker = M.Datepicker.init(eventStartElements, {
+            defaultDate: now,
+            onSelect: startCallback,
+        }).shift() || null;
+        const eventEndPicker = M.Datepicker.init(eventEndElements, {
+            defaultDate: now,
+            onSelect: endCallback,
+        }).shift() || null;
+        const eventStartInputElement = document.getElementById('event-start');
+        const eventEndInputElement = document.getElementById('event-end');
+        this.setState({
+            eventStartInputElement,
+            eventEndInputElement,
+            eventStartPicker,
+            eventEndPicker,
+            eventStart: now,
+            eventEnd: now,
+        });
+    }
+
+    componentWillUnmount() {
+        this.state.eventStartPicker?.destroy();
+        this.state.eventEndPicker?.destroy();
+    }
+
+    handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+        this.setState({
+            title: e.target.value,
+        });
+    }
+
+    handleSubmit = (e: MouseEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const timestamp = Date.now();
+        if (this.state.title.length) {
+            this.props.create(this.props.authenticatedUser, {
+                title: this.state.title,
+                eventStart: this.state.eventStart,
+                eventEnd: this.state.eventEnd,
+                authorId: this.props.authenticatedUser.authorId,
+                createdDate: new Date(timestamp),
+                modifiedDate: new Date(timestamp),
+                timelineId: -timestamp,
+            }, this.props.isOffline);
+        }
+        const newNow = new Date();
+        const { eventStartInputElement, eventEndInputElement } = this.state;
+        // this condition will always be satisfied, but it's necessary to quiet the compilation warning
+        if (eventStartInputElement !== null && eventEndInputElement !== null) {
+            eventStartInputElement.value = '';
+            eventEndInputElement.value = '';
+        }
+        this.setState({
+            title: '',
+            eventStart: newNow,
+            eventEnd: newNow,
+        });
+    }
+
+    render() {
+        const { timelineErrorState } = this.props;
+        return (
+            <div className="timelines-create card-panel story white row">
+                <form onSubmit={this.handleSubmit} className="">
+                    <h6>New Timeline</h6>
+                    <div className="divider"></div>
+                    <div className="input-field">
+                        <input id="title" type="text" className="validate" onChange={this.handleInputChange} value={this.state.title} required autoFocus />
+                        <label htmlFor="title">Title</label>
+                    </div>
+                    <div className="input-field">
+                        <input id="event-start" type="text" className="datepicker datepicker-newtimeline-cc-event-start" required />
+                        <label htmlFor="event-start">Event Start</label>
+                    </div>
+                    <div className="input-field">
+                        <input id="event-end" type="text" className="datepicker datepicker-newtimeline-cc-event-end" required />
+                        <label htmlFor="event-end">Event End</label>
+                    </div>
+                    <div className="input-field center">
+                        <button className="btn-small">Create</button>
+                        <div className="red-text center">
+                            { timelineErrorState && timelineErrorState.displayErrorMessage &&
+                                <p>{timelineErrorState.displayErrorMessage}</p>
+                            }
+                        </div>
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default localConnector(NewTimelineCard);
--- penman-ui/src/components/timeline/TimelinesPage.tsx
+++ penman-ui/src/components/timeline/TimelinesPage.tsx
@@ -3,9 +3,10 @@ import { connect, ConnectedProps } from 'react-redux';
 import { push } from 'connected-react-router';
 import { IRootState, IAuthenticatedUser, INewTimeline, ITimeline } from '../../store/types';
 import { isAuthTokenExpired, refreshToken } from '../../store/actions/authActions';
-import bookImg from '../../img/book.jpg';
 import { create, read, readAll, update, deleteEntity } from '../../store/actions/timelineActions';
 import { defaultDate, timelineConstants } from '../../config/constants';
+import NewTimelineCard from './NewTimelineCard';
+// import TimelineCard from './TimelineCard';
 
 const mapStateToProps = (state: IRootState) => {
     return {
@@ -44,27 +45,33 @@ class TimelinesPage extends Component<Props> {
 
     render() {
         const { authenticatedUser, isOffline, isTokenExpired } = this.props;
-        // const loaderDisplayStyle = (this.props.isLoading && this.props.timelinesCount === 0 ? 'block' : 'none');
+        const loaderDisplayStyle = (this.props.isLoading && this.props.timelinesCount === 0 ? 'block' : 'none');
         if (isTokenExpired(authenticatedUser, isOffline)) {
             push('/signin');
         }
         return (
-            <div className="dashboard container">
-                <div className="dashboard-work-area stories container grey-text text-darken-1 col s12 m6">
-                    {/** Extract cards to component */}
-                    <div className="card-panel story white row">
-                        <img src={bookImg} alt="A book" />
-                        <div className="story-details">
-                            <div className="story-title">Some Title</div>
-                            <div className="story-contents">The makings of a story.</div>
+            <div className="timelines container">
+                <div className="timelines-work-area stories container grey-text text-darken-1 col s12 m6">
+                    <NewTimelineCard />
+                    <div className="timelines">
+                        <div className="blue-text" style={{display: loaderDisplayStyle}}>
+                            <div className="preloader-wrapper big active">
+                                <div className="spinner-layer"><div className="circle" /></div>
+                                <div className="gap-patch"><div className="circle" /></div>
+                                <div className="circle-clipper right"><div className="circle"></div></div>
+                            </div>
                         </div>
-                        <div className="story-delete secondary-content">
-                            <i className="material-icons">delete_outline</i>
-                        </div>
+                        {/* {Object.values(this.props.timelines).reverse().map(timeline =>
+                            <TimelineCard
+                                key={`timelineId:${timeline.timelineId}`}
+                                timeline={timeline}
+                                user={authenticatedUser}
+                                isOffline={isOffline}
+                                update={this.props.update}
+                                deleteEntity={this.props.deleteEntity}
+                            />
+                        )} */}
                     </div>
-                </div>
-                <div className="dashboard-notifications-area col s12 m5 offset-m1">
-                    <span>Example notification</span>
                 </div>
             </div>
         );
