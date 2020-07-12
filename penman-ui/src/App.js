5355bf761daadfb206eaf5661a3b46f8912c13aa
mathlander <daniel.uribe@gmail.com>
2020-07-12 17:57:17 -0500

    Connected the app to redux.  Added WelcomePage.

--- penman-ui/src/App.js
+++ penman-ui/src/App.js
@@ -1,25 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
+import { ConnectedRouter } from 'connected-react-router';
+import { Switch, Route } from 'react-router-dom';
+import { history } from './store/reducers/rootReducer';
+// import OfflineManager from './components/offline/OfflineManager';
+// import Toaster from './components/toaster/Toaster';
+import Navbar from './components/layout/Navbar';
+// import DashboardPage from './components/dashboard/DashboardPage';
+// import PromptsPage from './components/prompt/PromptsPage';
+// import SignInPage from './components/auth/SignInPage';
+// import SignUpPage from './components/auth/SignUpPage';
+import WelcomePage from './components/welcome/WelcomePage';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ConnectedRouter history={history}>
+      <div className="App">
+        {/* <OfflineManager />
+        <Toaster /> */}
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={WelcomePage} />
+          {/* <Route path="/signin" component={SignInPage} />
+          <Route path="/signup" component={SignUpPage} /> */}
+          <Route path="/welcome" component={WelcomePage} />
+          {/* <Route path="/dashboard" component={DashboardPage} />
+          <Route path="/prompts" component={PromptsPage} /> */}
+        </Switch>
+      </div>
+    </ConnectedRouter>
   );
 }
 
--- penman-ui/src/index.js
+++ penman-ui/src/index.js
@@ -3,15 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { routerMiddleware } from 'connected-react-router';
+import rootReducer, { history } from './store/reducers/rootReducer';
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(
+    thunk,
+    routerMiddleware(history)
+  )
+);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
